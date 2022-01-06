e438056457dc733baa86402f4a355818f18b3ea7
Max Schweikart <hello@maxschweik.art>
2022-01-06 14:50:33 +0100

    add drag and drop support for germane cards

--- Assets/UI/UIController.cs
+++ Assets/UI/UIController.cs
@@ -1,29 +1,109 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+[RequireComponent(typeof(UIDocument))]
 public class UIController : MonoBehaviour
 {
-    private bool playing = false;
-    private Button playButton;
-    private VisualTreeAsset tree;
+    public float Speed = 10;
+
+    private bool _playing = false;
+    public bool Playing
+    {
+        get => _playing;
+        private set
+        {
+            _playing = value;
+            UpdatePlayButton();
+        }
+    }
+
+    private Button _playButton;
+    private Slider _timeSlider;
+    private ScrollView _germanSelectorScrollView;
+
+    private VisualElement _currentGermanSelectorDrag = null;
+
+    public GameObject DragGhost;
 
     void Start()
     {
-        playButton.RegisterCallback<MouseDownEvent>(OnPlayButtonClick);
+        var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
+
+        _playButton = rootVisualElement.Q<Button>("PlayButton");
+        _playButton.RegisterCallback<ClickEvent>(OnPlayButtonClick);
+
+        _timeSlider = rootVisualElement.Q<Slider>("TimeSlider");
+        _timeSlider.RegisterCallback<ChangeEvent<float>>(OnSliderValueChanged);
+
+        _germanSelectorScrollView = rootVisualElement.Q<ScrollView>("GermanSelector");
+        foreach (var germanSelector in _germanSelectorScrollView.contentContainer.Children())
+        {
+            Debug.Log(germanSelector.name);
+            germanSelector.RegisterCallback<MouseDownEvent>(evt => OnGermanSelectorDragStart(evt, germanSelector), TrickleDown.TrickleDown);
+            germanSelector.RegisterCallback<MouseUpEvent>(evt => OnGermanSelectorDragStop(evt, germanSelector));
+            germanSelector.RegisterCallback<MouseMoveEvent>(evt => OnGermanSelectorDragMove(evt, germanSelector));
+        }
     }
 
-    private void OnPlayButtonClick(MouseDownEvent evt)
+    private void OnGermanSelectorDragStart(MouseDownEvent evt, VisualElement germanSelector)
     {
-        playing = !playing;
-        playButton.text = playing ? "Pause" : "Play";
+        _currentGermanSelectorDrag = germanSelector;
+        DragGhost.SetActive(true);
+        Debug.Log("Dragging start: " + germanSelector.name);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGermanSelectorDragStop(MouseUpEvent evt, VisualElement germanSelector)
     {
-        
+        _currentGermanSelectorDrag = null;
+        Instantiate(DragGhost);
+        DragGhost.SetActive(false);
+        Debug.Log("Dragging stop: " + germanSelector.name);
+    }
+
+    private void OnGermanSelectorDragMove(MouseMoveEvent evt, VisualElement germanSelector)
+    {
+        if (_currentGermanSelectorDrag != null)
+        {
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out var hit);
+            DragGhost.transform.position = hit.point;
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+        }
+    }
+
+    private void Update()
+    {
+        if (Playing)
+        {
+            var newTime = _timeSlider.value + Speed * Time.deltaTime;
+            if (newTime >= _timeSlider.highValue)
+            {
+                newTime = _timeSlider.highValue;
+                Playing = false;
+            }
+
+            _timeSlider.value = newTime;
+        }
+    }
+
+    private void UpdatePlayButton()
+    {
+        _playButton.text = (Playing ? "Pause" : "Play") + " (" + _timeSlider.value + ")";
+    }
+
+    private void OnPlayButtonClick(ClickEvent evt)
+    {
+        if (!Playing && Math.Abs(_timeSlider.value - _timeSlider.highValue) < 0.1)
+        {
+            _timeSlider.value = _timeSlider.lowValue;
+        }
+
+        Playing = !Playing;
+    }
+
+    private void OnSliderValueChanged(ChangeEvent<float> evt)
+    {
+        UpdatePlayButton();
     }
 }
