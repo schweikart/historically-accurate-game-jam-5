8ae301cfe3c6dc60b06e371beb8dedeeee8c1229
PhilippSeidel00 <spam@philipp-seidel.de>
2022-01-06 12:11:06 +0100

    added FOV stuff

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public float moveSpeed = 6;
+
+    Rigidbody rigidBody;
+    Camera viewCamera;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+}
