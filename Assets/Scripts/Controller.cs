882aae35a2adc762ae12b63deede31ec8f3d9f19
PhilippSeidel00 <spam@philipp-seidel.de>
2022-01-08 16:47:41 +0100

    level1 :)

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -21,9 +21,7 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
-        transform.LookAt(mousePos + Vector3.up * transform.position.y);
-        velocity = new Vector3(Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal")).normalized * moveSpeed;
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
     }
 
     private void FixedUpdate()
