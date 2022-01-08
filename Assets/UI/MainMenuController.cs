1d136c24d0f157e6747bd00fec56d358c0c156b1
Max Schweikart <hello@maxschweik.art>
2022-01-08 23:51:22 +0100

    implement historical background

--- Assets/UI/MainMenuController.cs
+++ Assets/UI/MainMenuController.cs
@@ -7,13 +7,38 @@ namespace Arminius
     {
         public GameController gameController;
 
+        private bool _backgroundHistoryShown;
+
+        private bool BackgroundHistoryShown
+        {
+            get => _backgroundHistoryShown;
+            set
+            {
+                _backgroundHistoryShown = value;
+                if (_backgroundHistoryShown)
+                {
+                    _backgroundHistoryPanel.style.opacity = 1;
+                }
+                else
+                {
+                    _backgroundHistoryPanel.style.opacity = 0;
+                }
+            }
+        }
+
+        private VisualElement _backgroundHistoryPanel;
+
         void OnEnable()
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
 
+            _backgroundHistoryPanel = root.Q<VisualElement>("BackgroundTextContainer");
+            BackgroundHistoryShown = false;
+
             root.Q<Button>("PlayButton").RegisterCallback<ClickEvent>(OnPlayButtonClick);
-            root.Q<Button>("BackgroundButton").RegisterCallback<ClickEvent>(OnBackgroundButtonClick);
+            root.Q<Button>("BackgroundButton").RegisterCallback<ClickEvent>(evt => BackgroundHistoryShown = !BackgroundHistoryShown);
             root.Q<Button>("ExitButton").RegisterCallback<ClickEvent>(OnExitButtonClick);
+            root.Q<Button>("LearnMoreButton").RegisterCallback<ClickEvent>(evt => Application.OpenURL("https://en.wikipedia.org/wiki/Battle_of_the_Teutoburg_Forest"));
         }
 
         private void OnPlayButtonClick(ClickEvent evt)
